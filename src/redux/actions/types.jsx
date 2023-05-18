5ed8913fd2fd33966f82d007eaedb114686e45e6
Kai-Itakura <itakai199969@gmail.com>
2023-05-18 12:19:23 +0900

    actionとtypeの実装

---
+++ src/redux/actions/types.jsx
@@ -0,0 +1,14 @@
+// 数字がクリックされたとき
+export const INPUT_NUMBER = 'INPUT_NUMBER'
+
+// 演算子ボタンがクリックされたとき
+export const PLUS = 'PLUS'
+export const MINUS = 'MINUS'
+export const MULTIPLY = 'MULTIPLY'
+export const DIVIDE = 'DIVIDE'
+
+// イコールがクリックされたとき
+export const EQUAL = 'EQUAL'
+
+// ACボタンがクリックされたとき
+export const CLEAR = 'CLEAR'
